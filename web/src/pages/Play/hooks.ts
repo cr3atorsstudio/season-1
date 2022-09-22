7fc090406b3b408c15106a78b443ca5772270aea
Eiko0705 <eiko.imai.070502@gmail.com>
2022-09-22 21:23:00 +0900

    feat: added func for changing input word to hiragana

--- web/src/pages/Play/hooks.ts
+++ web/src/pages/Play/hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { actions, initialState, reducer } from "reducers/play";
 import kuromoji from "kuromoji";
 
@@ -6,16 +6,18 @@ const { setLastWord, setInputWord, verifyJapaneseWord, setError } = actions;
 
 const useHandleAction = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [hiraganaInputWord, setHiraganaInputWord] = useState("");
 
   const handleWordChange = (input: string) => {
     dispatch(setInputWord(input));
   };
 
   const handleOnClick = () => {
-    if (state.inputWord, state.lastWord) {
-      let lastLetter = state.lastWord.slice(-1);
+
+    // 前の単語が特殊文字で終了する場合の最終文字の変形処理
+    if (state.lastWord) {
+      let lastLetter: string = state.lastWord.slice(-1);
       
-      // 前の単語が特殊文字で終了する場合の最終文字の変形処理
       if (lastLetter === 'ー') {
         lastLetter = state.lastWord.slice(0, -1).slice(-1);
       }
@@ -52,22 +54,41 @@ const useHandleAction = () => {
             break;
         default:
       }
+    }
 
-      // kuromojiを使って入力単語をひらがなに直す処理
-      kuromoji.builder({ dicPath: "/dict" }).build((err: any, tokenizer: any) => {
-        if(err){
-          console.log(err);
-        } else {
-          const tokens = tokenizer.tokenize(state.inputWord);
-          console.log(tokens);
-          if (tokens.length === 0 || tokens[0]['word_type'] === 'UNKNOWN') {
-            return;
-          }
-          if (tokens.length === 1 && tokens[0].word_type === "KNOWN") {
-            dispatch(verifyJapaneseWord(true));
-          }
-        }
-      })
+    if(state.inputWord){
+      // kuromojiを使って入力単語をひらがなに変換する処理
+      const changeToHiragana = (text: string) => {
+        return new Promise<string> ((resolve, reject) => {
+          kuromoji.builder({ dicPath: "/dict" }).build((error: Error, tokenizer: any) => {
+            if(error){
+              reject(error);
+              return;
+            } else {
+              const tokens: any[] = tokenizer.tokenize(text);
+              console.log(tokens);
+              if (tokens.length === 0 || tokens.length > 1 || tokens[0].word_type === 'UNKNOWN') {
+                return;
+              }
+              if (!tokens[0].reading) {
+                return;
+              }
+              let reading: string = tokens[0].reading;
+              dispatch(verifyJapaneseWord(true));
+              const hiragana: string = reading.replace(/[\u30a1-\u30f6]/g, function(match: string) {
+                const chr = match.charCodeAt(0) - 0x60;
+                return String.fromCharCode(chr);
+              });
+              resolve(hiragana);
+            }
+          })
+        })
+      }
+
+      changeToHiragana(state.inputWord)
+        .then((data: string): void => {
+          setHiraganaInputWord(data); 
+        })
     }
   }
   
