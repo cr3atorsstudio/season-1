64aa89daee4ae5f623c180cc224afd679461027d
mihoogura <miho@ryu.games>
2022-10-02 16:15:24 +0900

    Add connect wallet button & rename howtoplay to about page

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,28 +1,134 @@
+import { Link } from "react-router-dom";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 export const Navbar = () => {
   return (
     <div>
-      <nav className="px-2 py-8 sm:px-4">
-        <div className="justify-between items-center px-2 flex flex-nowrap">
-          <a className="flex w-56 items-center" href="">
-            <img 
-              src="public/images/creatorsstudio.jpeg"
-              alt="Creators Studio Logo"
-              className="ml-10 mr-5 sm:h-11 object-contain h-full rounded-lg"
+      <nav className='px-2 py-8 sm:px-4'>
+        <div className='flex flex-nowrap items-center justify-between px-2'>
+          <a className='flex w-56 items-center' href=''>
+            <img
+              src='public/images/creatorsstudio.jpeg'
+              alt='Creators Studio Logo'
+              className='ml-10 mr-5 h-full rounded-lg object-contain sm:h-11'
             />
-            <span className="self-center text-xl whitespace-nowrap text-white" >Creators Studio</span>
+            <Link
+              className='self-center whitespace-nowrap text-xl text-white'
+              to={"/"}
+            >
+              Creators Studio
+            </Link>
           </a>
           <div>
-            <ul className="flex lg:flex-row list-none ml-auto">
+            <ul className='ml-auto flex list-none lg:flex-row'>
               <li>
-                <a className="flex w-40 px-9 py-2 items-center text-white hover:opacity-75">説明</a>
+                <Link
+                  className='flex w-40 items-center px-9 py-2 text-white hover:opacity-75'
+                  to={"/about"}
+                >
+                  説明
+                </Link>
               </li>
               <li>
-                <a className="flex w-40 px-9 py-2 items-center text-white hover:opacity-75">参加する</a>
+                <Link
+                  className='flex w-40 items-center px-9 py-2 text-white hover:opacity-75'
+                  to={"/play"}
+                >
+                  参加する
+                </Link>
               </li>
             </ul>
           </div>
-          <div className="w-56">
-            <button className="bg-transparent text-white py-2 px-4 border border-pink-400 rounded-full" >ウォレット接続</button>
+          <div className='w-56'>
+            <ConnectButton.Custom>
+              {({
+                account,
+                chain,
+                openAccountModal,
+                openChainModal,
+                openConnectModal,
+                mounted,
+              }) => {
+                // Note: If your app doesn't use authentication, you
+                // can remove all 'authenticationStatus' checks
+                const ready = mounted;
+                const connected = ready && account && chain;
+
+                return (
+                  <div
+                    {...(!ready && {
+                      "aria-hidden": true,
+                      style: {
+                        opacity: 0,
+                        pointerEvents: "none",
+                        userSelect: "none",
+                      },
+                    })}
+                  >
+                    {(() => {
+                      if (!connected) {
+                        return (
+                          <button
+                            onClick={openConnectModal}
+                            type='button'
+                            className='rounded-3xl border border-accent p-3'
+                          >
+                            Connect Wallet
+                          </button>
+                        );
+                      }
+
+                      if (chain.unsupported) {
+                        return (
+                          <button onClick={openChainModal} type='button'>
+                            Wrong network
+                          </button>
+                        );
+                      }
+
+                      return (
+                        <div style={{ display: "flex", gap: 12 }}>
+                          <button
+                            onClick={openChainModal}
+                            style={{ display: "flex", alignItems: "center" }}
+                            type='button'
+                          >
+                            {chain.hasIcon && (
+                              <div
+                                style={{
+                                  background: chain.iconBackground,
+                                  width: 12,
+                                  height: 12,
+                                  borderRadius: 999,
+                                  overflow: "hidden",
+                                  marginRight: 4,
+                                }}
+                              >
+                                {chain.iconUrl && (
+                                  <img
+                                    alt={chain.name ?? "Chain icon"}
+                                    src={chain.iconUrl}
+                                    style={{ width: 12, height: 12 }}
+                                  />
+                                )}
+                              </div>
+                            )}
+                            {chain.name}
+                          </button>
+
+                          <button onClick={openAccountModal} type='button'>
+                            {account.displayName}
+                            {account.displayBalance
+                              ? ` (${account.displayBalance})`
+                              : ""}
+                          </button>
+                        </div>
+                      );
+                    })()}
+                  </div>
+                );
+              }}
+            </ConnectButton.Custom>
           </div>
         </div>
       </nav>
