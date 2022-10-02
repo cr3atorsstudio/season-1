c7cf405cc6a60c59a48a05e0657e765f9f9ee80a
mihoogura <miho@ryu.games>
2022-10-02 16:31:17 +0900

    Remove nonuse import

--- web/src/pages/Play/hooks.ts
+++ web/src/pages/Play/hooks.ts
@@ -2,7 +2,7 @@ import { useEffect, useReducer } from "react";
 import { actions, initialState, reducer } from "reducers/play";
 import kuromoji from "kuromoji";
 
-const { setLastWord, setInputWord, verifyJapaneseWord, setError } = actions;
+const { setInputWord, verifyJapaneseWord } = actions;
 
 const useHandleAction = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -17,96 +17,100 @@ const useHandleAction = () => {
 
     // 前の単語が特殊文字で終了する場合の最終文字の変形処理
     if (state.lastWord) {
-      
-      if (lastCharacter === 'ー') {
+      if (lastCharacter === "ー") {
         lastCharacter = state.lastWord.slice(0, -1).slice(-1);
       }
-      switch(lastCharacter) {
-        case 'ぁ':
-            lastCharacter = 'あ';
-            break;
-        case 'ぃ':
-            lastCharacter = 'い';
-            break;
-        case 'ぅ':
-            lastCharacter = 'う';
-            break;
-        case 'ぇ':
-            lastCharacter = 'え';
-            break;
-        case 'ぉ':
-            lastCharacter = 'お';
-            break;
-        case 'っ':
-            lastCharacter = 'つ';
-            break;
-        case 'ゃ':
-            lastCharacter = 'や';
-            break;
-        case 'ゅ':
-            lastCharacter = 'ゆ';
-            break;
-        case 'ょ':
-            lastCharacter = 'よ';
-            break;
-        case 'ゎ':
-            lastCharacter = 'わ';
-            break;
+      switch (lastCharacter) {
+        case "ぁ":
+          lastCharacter = "あ";
+          break;
+        case "ぃ":
+          lastCharacter = "い";
+          break;
+        case "ぅ":
+          lastCharacter = "う";
+          break;
+        case "ぇ":
+          lastCharacter = "え";
+          break;
+        case "ぉ":
+          lastCharacter = "お";
+          break;
+        case "っ":
+          lastCharacter = "つ";
+          break;
+        case "ゃ":
+          lastCharacter = "や";
+          break;
+        case "ゅ":
+          lastCharacter = "ゆ";
+          break;
+        case "ょ":
+          lastCharacter = "よ";
+          break;
+        case "ゎ":
+          lastCharacter = "わ";
+          break;
         default:
       }
     }
 
-    if(state.inputWord){
+    if (state.inputWord) {
       // kuromojiを使って入力単語をひらがなに変換する処理
       const changeToHiragana = (text: string) => {
-        return new Promise<string> ((resolve, reject) => {
-          kuromoji.builder({ dicPath: "/dict" }).build((error: Error, tokenizer: any) => {
-            if(error){
-              reject(error);
-              return;
-            } else {
-              const tokens: any[] = tokenizer.tokenize(text);
-              console.log(tokens);
-              if (tokens.length === 0 || tokens.length > 1) {
-                console.log("Please input one word.");
+        return new Promise<string>((resolve, reject) => {
+          kuromoji
+            .builder({ dicPath: "/dict" })
+            .build((error: Error, tokenizer: any) => {
+              if (error) {
+                reject(error);
                 return;
+              } else {
+                const tokens: any[] = tokenizer.tokenize(text);
+                console.log(tokens);
+                if (tokens.length === 0 || tokens.length > 1) {
+                  console.log("Please input one word.");
+                  return;
+                }
+                if (tokens[0].word_type === "UNKNOWN") {
+                  console.log("Please input KNOWN word.");
+                  return;
+                }
+                if (!tokens[0].reading) {
+                  return;
+                }
+                let reading: string = tokens[0].reading;
+                const hiragana: string = reading.replace(
+                  /[\u30a1-\u30f6]/g,
+                  function (match: string) {
+                    const chr = match.charCodeAt(0) - 0x60;
+                    return String.fromCharCode(chr);
+                  }
+                );
+                resolve(hiragana);
               }
-              if (tokens[0].word_type === 'UNKNOWN') {
-                console.log("Please input KNOWN word.");
-                return;
-              }
-              if (!tokens[0].reading) {
-                return;
-              }
-              let reading: string = tokens[0].reading;
-              const hiragana: string = reading.replace(/[\u30a1-\u30f6]/g, function(match: string) {
-                const chr = match.charCodeAt(0) - 0x60;
-                return String.fromCharCode(chr);
-              });
-              resolve(hiragana);
-            }
-          })
-        })
-      }
-      
+            });
+        });
+      };
+
       // 前の単語の最終語句と続いているか確認し、繋がっていればstateをtrueに変更する
       changeToHiragana(state.inputWord)
-      .then((data: string): void => {
-        console.log(data);
-        hiraganaInputWord = data;
-      })
-      .then((): void => {
-        if (lastCharacter === hiraganaInputWord.slice(0, 1)) {
-          dispatch(verifyJapaneseWord(true));
-          console.log("the input word can follow the previous word!");
-        };
-        if (lastCharacter !== hiraganaInputWord.slice(0, 1)) {
-          console.log("the input word cannot follow the previous word!")
-        };
-      })
+        .then((data: string): void => {
+          console.log(data);
+          hiraganaInputWord = data;
+        })
+        .then((): void => {
+          if (lastCharacter === hiraganaInputWord.slice(0, 1)) {
+            dispatch(verifyJapaneseWord(true));
+            console.log("the input word can follow the previous word!");
+          }
+          if (lastCharacter !== hiraganaInputWord.slice(0, 1)) {
+            console.log("the input word cannot follow the previous word!");
+          }
+        });
     }
-  }
-  
+  };
+
   useEffect(() => {
     // TODO: 現状の最後の単語をfetchするfunctionを入れる
   });
@@ -116,7 +120,7 @@ const useHandleAction = () => {
   return {
     ...state,
     handleWordChange: handleWordChange,
-    handleOnClick: handleOnClick
+    handleOnClick: handleOnClick,
   };
 };
 
