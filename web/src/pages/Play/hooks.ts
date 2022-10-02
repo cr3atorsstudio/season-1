4dd6cecc18c2f7275ee476e02d0c3f1003a4671d
Eiko0705 <eiko.imai.070502@gmail.com>
2022-10-02 19:36:38 +0900

    fix: fixed some bags

--- web/src/pages/Play/hooks.ts
+++ web/src/pages/Play/hooks.ts
@@ -62,57 +62,49 @@ const useHandleAction = () => {
       }
     }
 
-    if (state.inputWord) {
-      // kuromojiを使って入力単語をひらがなに変換する処理
-      const changeToHiragana = (text: string) => {
-        return new Promise<string>((resolve, reject) => {
-          kuromoji
-            .builder({ dicPath: "/dict" })
-            .build((error: Error, tokenizer: any) => {
-              if (error) {
-                reject(error);
-                return;
-              } else {
-                const tokens: any[] = tokenizer.tokenize(text);
-                console.log(tokens);
-                if (tokens.length === 0 || tokens.length > 1) {
-                  dispatch(checkWordError(true));
-                  dispatch(setWordErrorMessage("一単語だけ入力してください。"));
-                  return;
-                }
-                if (tokens[0].word_type === "UNKNOWN") {
-                  dispatch(checkWordError(true));
-                  dispatch(
-                    setWordErrorMessage(
-                      "実際に存在する単語を入力してください。"
-                    )
-                  );
-                  return;
-                }
-                if (!tokens[0].reading) {
-                  dispatch(checkWordError(true));
-                  dispatch(
-                    setWordErrorMessage(
-                      "認識できませんでした。別の単語を入力してください。"
-                    )
-                  );
-                  return;
-                }
-                let reading: string = tokens[0].reading;
-                const hiragana: string = reading.replace(
-                  /[\u30a1-\u30f6]/g,
-                  function (match: string) {
-                    const chr = match.charCodeAt(0) - 0x60;
-                    return String.fromCharCode(chr);
-                  }
-                );
-                resolve(hiragana);
-              }
+    const changeToHiragana = (text: string) => {
+      return new Promise<string> ((resolve, reject) => {
+        kuromoji.builder({ dicPath: "/dict" }).build((error: Error, tokenizer: any) => {
+          if(error){
+            reject(error);
+            return;
+          } else {
+            const tokens: any[] = tokenizer.tokenize(text);
+            console.log(tokens);
+            if (tokens.length === 0 || tokens.length > 1) {
+              dispatch(checkWordError(true));
+              dispatch(setWordErrorMessage("一単語だけ入力してください。"));
+              return;
+            }
+            if (tokens[0].word_type === 'UNKNOWN') {
+              dispatch(checkWordError(true));
+              dispatch(setWordErrorMessage("実際に存在する単語を入力してください。"));
+              return;
+            }
+            if (!tokens[0].reading) {
+              dispatch(checkWordError(true));
+              dispatch(setWordErrorMessage("認識できませんでした。別の単語を入力してください。"));
+              return;
+            }
+            let reading: string = tokens[0].reading;
+            const hiragana: string = reading.replace(/[\u30a1-\u30f6]/g, function(match: string) {
+              const chr = match.charCodeAt(0) - 0x60;
+              return String.fromCharCode(chr);
             });
-        });
-      };
+            resolve(hiragana);
+          }
+        })
+      })
+    }
 
-      // 前の単語の最終語句と続いているか確認し、繋がっていればstateをtrueに変更する
+    // 入力が無い場合のエラー
+    if(!state.inputWord) {
+      dispatch(checkWordError(true));
+      dispatch(setWordErrorMessage("単語を入力してください。"));
+    }
+
+    // 前の単語の最終語句と続いているか確認し、繋がっていればstateをtrueに変更する
+    if(state.inputWord) {
       changeToHiragana(state.inputWord)
         .then((data: string): void => {
           console.log(data);
