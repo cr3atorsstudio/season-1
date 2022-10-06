59d42e57fddb1d2fccc948161548a37e4a1a70f9
Eiko0705 <eiko.imai.070502@gmail.com>
2022-10-07 02:14:49 +0900

    feat: adjust the layout

--- web/src/pages/About/About.tsx
+++ web/src/pages/About/About.tsx
@@ -13,20 +13,73 @@ const About = () => {
           leftControl={<ArrowLeftCircleIcon className="h-12 w-12 text-blue-500" />}
           rightControl={<ArrowRightCircleIcon className="h-12 w-12 text-blue-500" />}
         >
-          <div>
+          <div className='relative h-full'>
             <div className='flex flex-col items-center justify-center'>
-              <div className='mb-20 text-[30px] font-bold md:text-[60px]'>
-                <p>どんなアプリ？</p>
-              </div>
-              <p className='w-2/3 text-[10px] font-semibold md:text-[40px]'>
-                このアプリは、みんな知っている”しりとり”ゲームとジェネレイティブアートを組み合わせたDappです。
+              <p className='mt-10 mb-20 text-[30px] font-bold md:text-[60px]'>
+                どんなアプリ？
+              </p>
+              <p className='text-[10px] font-semibold md:text-[28px] leading-8 w-3/4'>
+                このアプリは、誰もが知っている”しりとり”ゲームとジェネレイティブアートを組み合わせた『DApp』です。<br/><br/>
+                ゲームの参加者みんなでつなげる”しりとり”は、ブロックチェーン上に保管されます。そして、一度保管された”しりとり”は、後から言葉を入れ替えたり、言葉の繋がりを変えることはできません。<br/>
+                さらに、言葉をつなげることで、NFTを利用した唯一無二のデジタルアートを手に入れることができます。<br/><br/>
+                どんどん”しりとり”をつなげて、あなただけのデジタルアートを手に入れてみましょう。
               </p>
             </div>
-            <div className='ml-64 flex items-end h-60'>
-              <p　className='text-[30px] md:text-[60px] font-nico '>
-                01/04
+
+            <p className='absolute left-36 bottom-16 text-[30px] font-bold md:text-[60px] font-nico'>
+              01<span className='text-gray-400'>/04</span>
+            </p>
+          </div>
+          <div className='relative h-full'>
+            <div className='flex flex-col items-center justify-center'>
+              <p className='mt-10 mb-20 text-[30px] font-bold md:text-[60px]'>
+                つなげる文字を考えよう！
+              </p>
+              <p className='text-[10px] font-semibold md:text-[26px] leading-8 w-3/4'>
+                このアプリは、誰もが知っている”しりとり”ゲームとジェネレイティブアートを組み合わせた『DApp』です。<br/><br/>
+                ゲームの参加者みんなでつなげる”しりとり”は、ブロックチェーン上に保管されます。そして、一度保管された”しりとり”は、後から言葉を入れ替えたり、言葉の繋がりを変えることはできません。<br/>
+                さらに、言葉をつなげることで、NFTを利用した唯一無二のデジタルアートを手に入れることができます。<br/><br/>
+                どんどん”しりとり”をつなげて、あなただけのデジタルアートを手に入れてみましょう。
               </p>
             </div>
+
+            <p className='absolute left-36 bottom-16 text-[30px] font-bold md:text-[60px] font-nico'>
+              02<span className='text-gray-400'>/04</span>
+            </p>
+          </div>
+          <div className='relative h-full'>
+            <div className='flex flex-col items-center justify-center'>
+              <p className='mt-10 mb-20 text-[30px] font-bold md:text-[60px]'>
+                実際につなげてみよう！
+              </p>
+              <p className='text-[10px] font-semibold md:text-[26px] leading-8 w-3/4'>
+                このアプリは、誰もが知っている”しりとり”ゲームとジェネレイティブアートを組み合わせた『DApp』です。<br/><br/>
+                ゲームの参加者みんなでつなげる”しりとり”は、ブロックチェーン上に保管されます。そして、一度保管された”しりとり”は、後から言葉を入れ替えたり、言葉の繋がりを変えることはできません。<br/>
+                さらに、言葉をつなげることで、NFTを利用した唯一無二のデジタルアートを手に入れることができます。<br/><br/>
+                どんどん”しりとり”をつなげて、あなただけのデジタルアートを手に入れてみましょう。
+              </p>
+            </div>
+
+            <p className='absolute left-36 bottom-16 text-[30px] font-bold md:text-[60px] font-nico'>
+              03<span className='text-gray-400'>/04</span>
+            </p>
+          </div>
+          <div className='relative h-full'>
+            <div className='flex flex-col items-center justify-center'>
+              <p className='mt-10 mb-20 text-[30px] font-bold md:text-[60px]'>
+                生成された絵を手に入れよう！
+              </p>
+              <p className='text-[10px] font-semibold md:text-[26px] leading-8 w-3/4'>
+                このアプリは、誰もが知っている”しりとり”ゲームとジェネレイティブアートを組み合わせた『DApp』です。<br/><br/>
+                ゲームの参加者みんなでつなげる”しりとり”は、ブロックチェーン上に保管されます。そして、一度保管された”しりとり”は、後から言葉を入れ替えたり、言葉の繋がりを変えることはできません。<br/>
+                さらに、言葉をつなげることで、NFTを利用した唯一無二のデジタルアートを手に入れることができます。<br/><br/>
+                どんどん”しりとり”をつなげて、あなただけのデジタルアートを手に入れてみましょう。
+              </p>
+            </div>
+
+            <p className='absolute left-36 bottom-16 text-[30px] font-bold md:text-[60px] font-nico'>
+              04<span className='text-gray-400'>/04</span>
+            </p>
           </div>
         </Carousel>
       </div>
