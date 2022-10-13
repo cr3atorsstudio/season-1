a77423f5864fd0e18e6c6a3110b708d2064496b0
Maoh <maoh.design@gmail.com>
2022-10-13 15:22:08 +0900

    feat: add mint function

--- contracts/contracts/shiritori.sol
+++ contracts/contracts/shiritori.sol
@@ -9,33 +9,42 @@ contract Shiritori is ERC1155 {
     // TODO: 「ん」で終わったとき: フロントと相談、isUsedNFlag?的なbooleanを渡す?
     /** 同じ単語は使える */
 
-    // func2: 最後のテキストを受け取る -> Generative Artを生成するAPIを呼ぶ -> mintする
     /* 最後のテキストを返す */
-    uint256 public constant LAST_WORD = 0;
-    // TODO: ↑これを返す関数が必要(func2)
-
+    uint256 public lastWard = 0;
+    // 次に使われるTokenIdを返す
+    uint256 public nextTokenId = 0;
     uint8 public amount = 1;
 
     // このjson周りをgenerative Artチームが作成中
     constructor() ERC1155("https://game.example/api/item/{id}.json") {
-        // _mint が mintする関数
-        _mint(msg.sender, LAST_WORD, 10**18, "");
+        // しりとりの一番最初の単語のミント　単語は仮
+        mint(0);
 
         _purchase(amount, LAST_WORD);
     }
 
     /**
-    * function1
-    * openzeplin見て
-    *
-    * @param amount the amount of tokens to purchase
-    * @param text the inputted word
-    */
+     * function1
+     * openzeplin見て
+     *
+     * @param amount the amount of tokens to purchase
+     * @param text the inputted word
+     */
     //?? amount 必要なの？
     function _purchase(uint256 amount, uint256 text) private {
         // exist text
         // require(text, "Exist the text");
         console.log(text);
         _mint(msg.sender, 0, amount, "");
+    }
+
+    // function2 単語を受け取る -> mintする
+    function mint(uint256 word) public {
+        // TODO: wordが最後の単語ときちんと繋がるのか判断（直コン対策用）
+
+        _mint(msg.sender, nextTokenId, 1, "");
+        // 変数を更新
+        nextTokenId += 1;
+        lastWard = word;
     }
 }
