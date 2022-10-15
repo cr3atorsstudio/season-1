11a873a12ebbd9eb6b80bc73d5f222396e302e40
mihoogura <miho@ryu.games>
2022-10-15 21:11:40 +0900

    Fix the type error

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -49,8 +49,6 @@ export const Navbar = () => {
                 openConnectModal,
                 mounted,
               }) => {
-                // Note: If your app doesn't use authentication, you
-                // can remove all 'authenticationStatus' checks
                 const ready = mounted;
                 const connected = ready && account && chain;
 
@@ -86,44 +84,46 @@ export const Navbar = () => {
                         );
                       }
 
-                      return (
-                        <div style={{ display: "flex", gap: 12 }}>
-                          <button
-                            onClick={openChainModal}
-                            style={{ display: "flex", alignItems: "center" }}
-                            type='button'
-                          >
-                            {chain.hasIcon && (
-                              <div
-                                style={{
-                                  background: chain.iconBackground,
-                                  width: 12,
-                                  height: 12,
-                                  borderRadius: 999,
-                                  overflow: "hidden",
-                                  marginRight: 4,
-                                }}
-                              >
-                                {chain.iconUrl && (
-                                  <img
-                                    alt={chain.name ?? "Chain icon"}
-                                    src={chain.iconUrl}
-                                    style={{ width: 12, height: 12 }}
-                                  />
-                                )}
-                              </div>
-                            )}
-                            {chain.name}
-                          </button>
+                      if (connected) {
+                        return (
+                          <div style={{ display: "flex", gap: 12 }}>
+                            <button
+                              onClick={openChainModal}
+                              style={{ display: "flex", alignItems: "center" }}
+                              type='button'
+                            >
+                              {chain.hasIcon && (
+                                <div
+                                  style={{
+                                    background: chain.iconBackground,
+                                    width: 12,
+                                    height: 12,
+                                    borderRadius: 999,
+                                    overflow: "hidden",
+                                    marginRight: 4,
+                                  }}
+                                >
+                                  {chain.iconUrl && (
+                                    <img
+                                      alt={chain.name ?? "Chain icon"}
+                                      src={chain.iconUrl}
+                                      style={{ width: 12, height: 12 }}
+                                    />
+                                  )}
+                                </div>
+                              )}
+                              {chain.name}
+                            </button>
 
-                          <button onClick={openAccountModal} type='button'>
-                            {account.displayName}
-                            {account.displayBalance
-                              ? ` (${account.displayBalance})`
-                              : ""}
-                          </button>
-                        </div>
-                      );
+                            <button onClick={openAccountModal} type='button'>
+                              {account.displayName}
+                              {account.displayBalance
+                                ? ` (${account.displayBalance})`
+                                : ""}
+                            </button>
+                          </div>
+                        );
+                      }
                     })()}
                   </div>
                 );
