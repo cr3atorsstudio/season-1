60e46c9f955147dd89466b184b7c4cf8523a4465
Maoh <maoh.design@gmail.com>
2022-10-19 12:24:51 +0900

    Add setURI function and fix typo

--- contracts/contracts/shiritori.sol
+++ contracts/contracts/shiritori.sol
@@ -9,42 +9,33 @@ contract Shiritori is ERC1155 {
     // TODO: 「ん」で終わったとき: フロントと相談、isUsedNFlag?的なbooleanを渡す?
     /** 同じ単語は使える */
 
+    // 認証用の一つ前の単語 //TODO: Change value
+    unit256 private authWord = 0;
     /* 最後のテキストを返す */
-    uint256 public lastWard = 0;
+    uint256 public lastWord = 0;
     // 次に使われるTokenIdを返す
     uint256 public nextTokenId = 0;
-    uint8 public amount = 1;
 
     // このjson周りをgenerative Artチームが作成中
-    constructor() ERC1155("https://game.example/api/item/{id}.json") {
-        // しりとりの一番最初の単語のミント　単語は仮
-        mint(0);
+    // TODO: change url
+    constructor() ERC1155("https://rlho.github.io/nft_sample/{id}.json") {}
 
-        _purchase(amount, LAST_WORD);
+    // setURI
+    function setURI(string newURI) public onlyOwner {
+        _setURI(newURI);
     }
 
-    /**
-     * function1
-     * openzeplin見て
-     *
-     * @param amount the amount of tokens to purchase
-     * @param text the inputted word
-     */
-    //?? amount 必要なの？
-    function _purchase(uint256 amount, uint256 text) private {
-        // exist text
-        // require(text, "Exist the text");
-        console.log(text);
-        _mint(msg.sender, 0, amount, "");
-    }
-
-    // function2 単語を受け取る -> mintする
-    function mint(uint256 word) public {
-        // TODO: wordが最後の単語ときちんと繋がるのか判断（直コン対策用）
-
+    // 単語と認証用の単語を受け取る -> mintする
+    function mint(uint256 word, unit256 authenticationWords) public {
+        if(authenticationWords !== authWord) {
+          return "Error: Authentication failed."
+        } else {
         _mint(msg.sender, nextTokenId, 1, "");
+
         // 変数を更新
+        authWord = lastWord;
         nextTokenId += 1;
-        lastWard = word;
+        lastWord = word;
+        }
     }
 }
