7583506d143448ac2c67fd640c94c3cda0a595f3
Risa Watanabe Harada <supikateriburu@gmail.com>
2022-10-20 01:56:40 +0900

    ✨ Create hidariParts

--- generative_art/index.js
+++ generative_art/index.js
@@ -2,22 +2,40 @@ const mergeImages = require('merge-images');
 const fs = require('fs');
 const { Canvas, Image } = require('canvas');
 
-mergeImages(['./images/base_image.png', './images/0/00.png'], {
-  Canvas: Canvas,
-  Image: Image
-})
-  .then(
-    b64 => {
-     return new Buffer.from(b64.replace(/^data:image\/\w+;base64,/, ""),'base64');
-    }
-  ).then(
-    decodedFile => {
-      fs.writeFile('combined.png', decodedFile, (err) => {
-        if(err){
-            console.log(err)
-        }else{
-            console.log('saved')
-        }
+async function generatePinkyImage(hidari, migi) {
+
+  const hidariParts = await imageParts(hidari)
+  const migiParts = await imageParts(migi)
+
+  console.log(hidariParts)
+
+  mergeImages(['./images/base_image.png', ...hidariParts], {
+    Canvas: Canvas,
+    Image: Image
+  })
+    .then(
+      b64 => {
+      return new Buffer.from(b64.replace(/^data:image\/\w+;base64,/, ""),'base64');
       }
-    )
-    })
+    ).then(
+      decodedFile => {
+        fs.writeFile('hidari.png', decodedFile, (err) => {
+          if(err){
+              console.log(err)
+          }else{
+              console.log('saved')
+          }
+        }
+      )
+      })
+    }
+
+async function imageParts(word) {
+  let wordArray = [];
+  for (let i = 0; i < word.length; i+=2) {
+    wordArray.push(`./images/${word.slice(i, i+2)}.png`);
+  }
+  return wordArray;
+}
+
+generatePinkyImage("000133", "302200")
