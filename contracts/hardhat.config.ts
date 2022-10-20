e71f6301d6027fea0b69e2e94991801a4ba46464
eri2490 <e.147xxx@gmail.com>
2022-10-20 19:59:40 +0900

    feat: update hardhat config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require('dotenv').config()
+const { API_URL, PRIVATE_KEY } = process.env;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
 
 export default config;
