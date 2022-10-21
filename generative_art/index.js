e95ccb084a914ab3db62e3554cac448f52f3c2e2
Risa Watanabe Harada <supikateriburu@gmail.com>
2022-10-21 23:13:20 +0900

    ✨ Generate art.png

--- generative_art/index.js
+++ generative_art/index.js
@@ -4,10 +4,10 @@ const { Canvas, Image } = require('canvas');
 
 async function generatePinkyImage(hidari, migi) {
 
-  const hidariParts = await imageParts(hidari)
-  const migiParts = await imageParts(migi)
+  const hidariParts = await imageParts(hidari, "left")
+  const migiParts = await imageParts(migi, "right")
 
-  mergeImages(['./images/base_image.png', ...hidariParts], {
+  mergeImages(['./images/base_image.png', ...hidariParts, ...migiParts], {
     Canvas: Canvas,
     Image: Image
   })
@@ -17,40 +17,21 @@ async function generatePinkyImage(hidari, migi) {
     }
   ).then(
     decodedFile => {
-      fs.writeFile('hidari.png', decodedFile, (err) => {
+      fs.writeFile('art.png', decodedFile, (err) => {
         if(err){
             console.log(err)
         }else{
-            console.log('hidari saved')
-        }
-      }
-    )
-  })
-
-  mergeImages(['./images/base_image.png', ...migiParts], {
-    Canvas: Canvas,
-    Image: Image
-  })
-  .then(
-    b64 => {
-    return new Buffer.from(b64.replace(/^data:image\/\w+;base64,/, ""),'base64');
-    }
-  ).then(
-    decodedFile => {
-      fs.writeFile('migi.png', decodedFile, (err) => {
-        if(err){
-            console.log(err)
-        }else{
-            console.log('migi saved')
+            console.log('saved')
         }
       }
     )
   })
 }
 
-async function imageParts(word) {
-  let wordArray = word.split("").map(x => `images/${x}.png`)
+async function imageParts(word, side) {
+  let wordArray = word.split("").map(x => `images/${side}/${x}.png`)
   return wordArray
 }
 
-generatePinkyImage("000133", "302200")
+// MEMO: sample
+// generatePinkyImage("いか", "かし")
