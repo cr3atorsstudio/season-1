217ec752f86cfb0de33c215c77705c9abab80dcd
riho.takagi <rihohearts@gmail.com>
2022-10-24 16:13:45 -0400

    Merge generative_art to web

--- web/src/generative_art/saveImage.ts
+++ web/src/generative_art/saveImage.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
+import { generatePinkyImage } from "./generatePinkyImage";
 import { genImages } from "./test";
-//import { sendFileToIPFS } from "./UploadToPinata";
 
 const imagesSaveDir = "./src/generativeArt/dist";
 
@@ -9,6 +9,7 @@ if (!fs.existsSync(imagesSaveDir)) {
 }
 
 const fileName = await genImages(220, 25, imagesSaveDir);
+const pinkyImage = await generatePinkyImage("いか", "かし");
 console.log(fileName);
 //TODO: Merge risacan's generative art
 //const readableStreamForFile = fs.createReadStream(`./dist/${fileName}.png`);
