23b163d786ea2853dbf8af1e4c9b4d7cf523fd7a
Eiko0705 <eiko.imai.070502@gmail.com>
2022-11-29 00:35:29 +0900

    feat: added arrow animation

--- web/src/pages/Play/Play.tsx
+++ web/src/pages/Play/Play.tsx
@@ -4,6 +4,7 @@ import { WordInput } from "components/WordInput";
 import useHandleAction from "./hooks";
 import { LoadingSpinner } from "components/Spinner";
 import { Notification } from "components/Notification";
+import { Player } from '@lottiefiles/react-lottie-player';
 
 const Play = () => {
   const {
@@ -31,9 +32,9 @@ const Play = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col items-center justify-center">
-        <div className="mt-20">
-          {isConnected ? (
+      {isConnected ? (
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col mt-20">
             <div>
               <p className="mb-10 text-center md:text-xl">
                   現在繋がっている単語数：{nextTokenId > 0 ? nextTokenId - 1 : 0}単語
@@ -41,47 +42,58 @@ const Play = () => {
               <p className="text-center md:text-xl">現在の最後の単語は...</p>
               <p className="font-nico text-[80px] md:text-[128px]">{lastWord}</p>
             </div>
-            ) : (
-            <div>
-              <p>
-                まずはあなたのWalletにつなげてください！
-              </p>
-            </div>
-            )}
-        </div>
-        <img
+          </div>
+          <img
           src="public/images/arrow.png"
           alt="arrow"
           className="h-20 md:h-auto"
-        />
-        <div className="flex flex-col items-center justify-center">
-          {isLoading ? (
-            <>
-              <LoadingSpinner />
-            </>
-          ) : (
-            <>
-              <WordInput
-                onChangeAction={handleWordChange}
-                lastWord={lastWord}
-              />
-              {hasWordError && (
-                <div className="mt-5 text-left text-red-500 md:text-xl">
-                  {errorTexts}
-                </div>
-              )}
-              <Button text={"つなげる"} onClick={handleOnClick} />
-            </>
+          />
+          <div className="flex flex-col items-center justify-center">
+            {isLoading ? (
+              <>
+                <LoadingSpinner />
+              </>
+            ) : (
+              <>
+                <WordInput
+                  onChangeAction={handleWordChange}
+                  lastWord={lastWord}
+                />
+                {hasWordError && (
+                  <div className="mt-5 text-left text-red-500 md:text-xl">
+                    {errorTexts}
+                  </div>
+                )}
+                <Button text={"つなげる"} onClick={handleOnClick} />
+              </>
+            )}
+          </div>
+
+          {process.show && (
+            <Notification
+              process={process}
+              onClickAction={handleOnClickNotification}
+            />
           )}
         </div>
-
-        {process.show && (
-          <Notification
-            process={process}
-            onClickAction={handleOnClickNotification}
-          />
+        ) : (
+        <>
+          <div className="flex justify-end mr-24">
+            <Player
+              src="https://assets7.lottiefiles.com/packages/lf20_0oummbbk.json"
+              className="player"
+              loop
+              autoplay
+              style={{ height: "150px", width: "150px" }}
+            />
+          </div>
+          <div className="mt-20">
+            <p　className="text-center mt-10 mb-20 text-[30px] font-bold md:text-[60px]">
+              まずはあなたのWalletにつなげてみましょう！
+            </p>
+          </div>
+        </>
         )}
-      </div>
     </>
   );
 };
