6cca806583e6c489ae12ebf7af9bb92c5b273244
eiko0705 <eiko.imai.070502@gmail.com>
2023-01-10 00:38:55 +0900

    fix: added the functionality for checking if wallet is connected

--- web/src/pages/Play/Play.tsx
+++ web/src/pages/Play/Play.tsx
@@ -4,6 +4,7 @@ import { WordInput } from "components/WordInput";
 import useHandleAction from "./hooks";
 import { LoadingSpinner } from "components/Spinner";
 import { Notification } from "components/Notification";
+import { Player } from '@lottiefiles/react-lottie-player';
 
 const Play = () => {
   const {
@@ -16,6 +17,7 @@ const Play = () => {
     isLoading,
     nextTokenId,
     process,
+    isConnected,
   } = useHandleAction();
 
   const errorTexts = wordErrorMessage.split("\n").map((text, index) => {
@@ -30,50 +32,69 @@ const Play = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col items-center justify-center">
-        <div className="mt-20">
-          <p className="mb-10 text-center md:text-xl">
-            現在繋がっている単語数：{nextTokenId > 0 ? nextTokenId - 1 : 0}単語
-          </p>
-          <p className="text-center md:text-xl">現在の最後の単語は...</p>
-          <p className="font-nico text-[80px] md:text-[128px]">{lastWord}</p>
-        </div>
-        <img src="/images/arrow.png" alt="arrow" className="h-20 md:h-auto" />
+      {isConnected ? (
         <div className="flex flex-col items-center justify-center">
-          {isLoading ? (
-            <>
-              <LoadingSpinner />
-            </>
-          ) : (
-            <>
-              <WordInput
-                onChangeAction={handleWordChange}
-                lastWord={lastWord}
-              />
-              {hasWordError && (
-                <div className="mt-5 text-left text-red-500 md:text-xl">
-                  {errorTexts}
-                </div>
-              )}
-              <Button text={"つなげる"} onClick={handleOnClick} />
-            </>
+          <div className="mt-20">
+            <p className="mb-10 text-center md:text-xl">
+              現在繋がっている単語数：{nextTokenId > 0 ? nextTokenId - 1 : 0}単語
+            </p>
+            <p className="text-center md:text-xl">現在の最後の単語は...</p>
+            <p className="font-nico text-[80px] md:text-[128px]">{lastWord}</p>
+          </div>
+          <img src="/images/arrow.png" alt="arrow" className="h-20 md:h-auto" />
+          <div className="flex flex-col items-center justify-center">
+            {isLoading ? (
+              <>
+                <LoadingSpinner />
+              </>
+            ) : (
+              <>
+                <WordInput
+                  onChangeAction={handleWordChange}
+                  lastWord={lastWord}
+                />
+                {hasWordError && (
+                  <div className="mt-5 text-left text-red-500 md:text-xl">
+                    {errorTexts}
+                  </div>
+                )}
+                <Button text={"つなげる"} onClick={handleOnClick} />
+              </>
+            )}
+          </div>
+          <a
+            href="https://opensea.io/collection/shiritorinft"
+            target="_blank"
+            className="mt-10 underline"
+          >
+            いままでに繋がったShiritori NFTをみる
+          </a>
+
+          {process.show && (
+            <Notification
+              process={process}
+              onClickAction={handleOnClickNotification}
+            />
           )}
         </div>
-        <a
-          href="https://opensea.io/collection/shiritorinft"
-          target="_blank"
-          className="mt-10 underline"
-        >
-          いままでに繋がったShiritori NFTをみる
-        </a>
-
-        {process.show && (
-          <Notification
-            process={process}
-            onClickAction={handleOnClickNotification}
-          />
-        )}
-      </div>
+      ) : (
+        <>
+          <div className="flex justify-end mr-24">
+            <Player
+              src="https://assets7.lottiefiles.com/packages/lf20_0oummbbk.json"
+              className="player"
+              loop
+              autoplay
+              style={{ height: "150px", width: "150px" }}
+            />
+          </div>
+          <div className="mt-20">
+            <p　className="text-center mt-10 mb-20 text-[30px] font-bold md:text-[60px]">
+              まずはあなたのWalletにつなげてみましょう！
+            </p>
+          </div>
+        </>
+      )}
     </>
   );
 };
